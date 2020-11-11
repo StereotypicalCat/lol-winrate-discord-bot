2f23aaa702fd7e4aeb225dc661b594ade8ac8e7e
Lucas Winther <lucasw89@live.dk>
2020-11-12 00:13:40 +0100

    Fixed node fetch command not working

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 const readline = require('readline');
+const fetch = require("node-fetch");
 
 const rl = readline.createInterface({
     input: process.stdin,
