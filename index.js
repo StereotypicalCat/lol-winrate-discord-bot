25d408b0a33f4ca8113dcef31881d2ae0b72673b
Lucas Winther <lucasw89@live.dk>
2022-09-22 23:09:45 +0200

    Initial port to new discord api and new backend

--- index.js
+++ index.js
@@ -1,3 +1,261 @@
+
+const { REST, Routes, SlashCommandBuilder } = require('discord.js');
+const axios = require('axios');
+const inputParsers = require('./inputParser');
+
+module.exports.removePrefix = (prefix) => removePrefix(prefix);
+
+let CLIENT_ID = '776207570482233376';
+let CLIENT_TOKEN = ""
+
+if (process.env.apikey == null){
+    console.log("Please specify an API key in your environment variables.");
+    process.exit(1);
+}
+else{
+    CLIENT_TOKEN = process.env.apikey;
+}
+
+console.log("Building command")
+const getWinrateTogetherBuilder = new SlashCommandBuilder()
+    .setName('getwinratetogether')
+    .setDescription('Get winrate together')
+    .addStringOption(option => option.setName('user1').setDescription('The first user to get winrate with').setRequired(true))
+    .addStringOption(option => option.setName('user2').setDescription('The second user of the winrate call').setRequired(false))
+/*    .addStringOption(option => option.setName('user3').setDescription('The second user of the winrate call').required(true))
+    .addStringOption(option => option.setName('user4').setDescription('The second user of the winrate call').required(true))
+    .addStringOption(option => option.setName('user4').setDescription('The second user of the winrate call').required(true))*/
+
+console.log("converting command to json")
+const jsonCommand = getWinrateTogetherBuilder.toJSON();
+
+const commands = [
+    {
+        name: 'ping',
+        description: 'Replies with Pong!',
+    },
+    jsonCommand
+
+];
+
+const rest = new REST({ version: '10' }).setToken(CLIENT_TOKEN);
+
+(async () => {
+    try {
+        console.log('Started refreshing application (/) commands.');
+
+        await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
+})();
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('Pong!');
+    }
+    else if (interaction.commandName === 'getwinratetogether'){
+        const result = await getWinrateTogether(interaction.options.getString('user1'), interaction.options.getString('user2'));
+
+        console.log("Finished awaiting result.")
+        console.log(result);
+        const {wins, losses} = result;
+
+
+        console.log(interaction.options.getString('user1'))
+        console.log(interaction.options.getString('user2'))
+        const users = "" + interaction.options.getString('user1') + (interaction.options.getString('user2') == null ? '' : ` and ${interaction.options.getString('user2')}`);
+
+        await interaction.reply(`The winrate of ${users} is ${wins} wins and ${losses} losses which ${((wins/(wins + losses)) * 100).toFixed(2)}%`);
+
+    }
+});
+
+const getWinrateTogether = async (user1, user2) => {
+
+    return new Promise(resolve => {
+
+        const requestUrl = `https://winrateapi.lucaswinther.info/api/winrate/SameTeam?TeamMate=${user1}${user2==null ? '' : `&TeamMate=${user2}`}`;
+
+        axios.get(requestUrl, {
+            params: {}
+        }).then(function (result) {
+            console.log("Here is the result:")
+            console.log(result.data);
+            console.log("There was a note: ");
+            console.log(result.data.note);
+
+            resolve({wins: result.data.wins,
+                losses: result.data.losses})
+
+        }).catch(function (error) {
+            //  message.channel.send("A username wasn't found, or there was a server problem, please try again later");
+            console.log("There was an error getting the winrate");
+            console.log(error);
+        }).then(function () {
+            // always executed
+            console.log("request finished")
+        });
+    });
+
+
+
+}
+
+
+
+let sendRealAnswer = (message, users, options) => {
+
+    let parameters = {}
+
+    // Adds the users
+    switch (users.length) {
+        case 1:
+            parameters = {
+                user1: users[0]
+            }
+            break;
+        case 2:
+            parameters = {
+                user1: users[0],
+                user2: users[1]
+            }
+            break;
+        case 3:
+            parameters = {
+                user1: users[0],
+                user2: users[1],
+                user3: users[2]
+            }
+            break;
+        case 4:
+            parameters = {
+                user1: users[0],
+                user2: users[1],
+                user3: users[2],
+                user4: users[3]
+            }
+            break;
+        case 5:
+            parameters = {
+                user1: users[0],
+                user2: users[1],
+                user3: users[2],
+                user4: users[3],
+                user5: users[4]
+            }
+            break;
+
+    }
+
+    // Adds the options
+    for (const [key, value] of Object.entries(options)){
+        switch (key) {
+            case "type":
+                // Bad input is handled on API
+                parameters.MatchSelector = value;
+                break;
+            case "matches":
+                let matchesIsValid = value >= 1 && value <= 200;
+                if (matchesIsValid){
+                    parameters.NoMatches = value;
+                } else{
+                    message.channel.send("Invalid --matches parameter. Please specify a value between 1 and 200");
+                }
+                break;
+            case "daysago":
+                let daysagoIsValid = value > 0;
+                if (daysagoIsValid){
+                    parameters.NoOfDays = value;
+                }
+        }
+    }
+
+    const requestUrl = `https://winrateapi.lucaswinther.info/api/WinRate/GetWinrateTogether/${users.length}`;
+
+    //
+    // Set a standard number of matches to look through, if youre are getting a winrate of just 1 person.
+    //
+    if (parameters.NoMatches === undefined && parameters.user2 === undefined){
+        parameters.NoMatches = 50;
+    }
+
+    console.log("Params: ")
+    console.log(parameters);
+
+    axios.get(requestUrl, {
+        params: parameters
+    }).then(function (result) {
+        console.log("Here is the result:")
+        console.log(result.data);
+        console.log("There was a note: ");
+        console.log(result.data.note);
+
+        // We hit API limit.
+        if (result.data?.note === '1'){
+            message.channel.send("Unable to fully satisfy the request, please try again later. The message below describes how far we looked.");
+        }
+        if (result.data?.note === '2'){
+            message.channel.send("Not enough matches played to satisfy the matches parameter");
+        }
+
+
+        let totalGames = result.data.wins + result.data.losses;
+
+        if (totalGames === 0){
+            message.channel.send(`The specified user(s) do not have any recent games together.`)
+            return
+        }
+
+        if (parameters.user2 !== undefined){
+            message.channel.send(`The winrate of ${parameters.user1}, ${parameters.user2}${parameters.user3 !== undefined ? `, ${parameters.user3}` : ""}${parameters.user4 !== undefined ? `, ${parameters.user4}` : ""}${parameters.user5 !== undefined ? `, ${parameters.user5}` : ""} is ${((result.data.wins/totalGames) * 100).toFixed(2)}% based on their recent ${totalGames} ${totalGames > 1 ? "games" : "game"} together.`)
+        }
+        else{
+            message.channel.send(`The winrate of ${parameters.user1} is ${((result.data.wins/totalGames) * 100).toFixed(2)}% based on their recent ${totalGames} games.`)
+        }
+
+
+
+
+        lastCall = new Date();
+    }).catch(function (error) {
+        message.channel.send("A username wasn't found, or there was a server problem, please try again later");
+        console.log("There was an error getting the winrate");
+        console.log(error);
+    }).then(function () {
+        // always executed
+    });
+}
+
+
+client.login(CLIENT_TOKEN);
+
+
+/*if (process.env.apikey == null){
+    console.log("Please specify an API key in your environment variables.");
+    process.exit(1);
+}
+else{
+    key = process.env.apikey;
+}*/
+
+// login to Discord with your app's token
+
+module.exports.removePrefix = (prefix) => removePrefix(prefix);
+
+
+/*
 const Discord = require('discord.js');
 const axios = require('axios');
 const inputParsers = require('./inputParser');
@@ -135,9 +393,9 @@ let sendRealAnswer = (message, users, options) => {
 
     const requestUrl = `https://winrateapi.lucaswinther.info/api/WinRate/GetWinrateTogether/${users.length}`;
 
-    /*
+    /!*
      * Set a standard number of matches to look through, if youre are getting a winrate of just 1 person.
-     */
+     *!/
     if (parameters.NoMatches === undefined && parameters.user2 === undefined){
         parameters.NoMatches = 50;
     }
@@ -205,3 +463,4 @@ client.login(key);
 module.exports.removePrefix = (prefix) => removePrefix(prefix);
 
 
+*/
