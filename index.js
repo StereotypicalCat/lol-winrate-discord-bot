86ab6ecde288de939bffb19391b86c71579a566b
Lucas Winther <lucasw89@live.dk>
2022-09-27 18:06:03 +0200

    WIP: Added player role and champion, started adding enemies

--- index.js
+++ index.js
@@ -42,12 +42,21 @@ const GameTypeToTextTwoWayMap = new TwoWayMap({
     'BAC': 'Unranked',
 });
 
+const RoleToTextTwoWayMap = new TwoWayMap({
+    'to': 'Toplane',
+    'su': "Support",
+    'ju': "Jungle",
+    'mi': "Midlane",
+    'bo': "Botlane",
+});
+
 // Per server cooldown between api calls
 // Increased if a request has high number of matches
 let cooldownPerMatchRequested = 1000;
 let server_cooldowns = new Map()
 
 console.log("Building command")
+
 
 
 const getWinrateTogetherBuilder = new SlashCommandBuilder()
@@ -57,6 +66,14 @@ const getWinrateTogetherBuilder = new SlashCommandBuilder()
         subcommand.setName('of_player')
             .setDescription('Gets the winrate of a player')
             .addStringOption(option => option.setName('user1').setDescription('The Username of the Player').setRequired(true))
+            .addStringOption(option => option.setName('user1_role').setDescription('The first user to get winrate with').setRequired(false).addChoices(
+                { name: 'Top', value: 'to' },
+                { name: 'Middle', value: 'mi'},
+                { name: 'Bottom', value: 'bo'},
+                { name: 'Jungle', value: 'ju'},
+                { name: 'Support', value: 'su'},
+            ))
+            .addStringOption(option => option.setName('user1_champ').setDescription('The first user to get winrate with').setRequired(false))
             .addStringOption(option => option.setName('gametypes').setDescription('The gamemodes to check.').setRequired(false).addChoices(
                 { name: 'Ranked Flex', value: 'Ranked Flex' },
                 { name: 'Ranked Solo/Duo', value: 'Ranked Solo/Duo' },
@@ -73,14 +90,130 @@ const getWinrateTogetherBuilder = new SlashCommandBuilder()
                 { name: 'Last 25', value: 25 },
                 { name: 'Last 50', value: 50 },
             )))
+    .addSubcommand(subcommand =>
+        subcommand.setName('of_player_against')
+            .setDescription('Gets the winrate of a player against specific users and champions in different roles')
+            .addStringOption(option => option.setName('user1').setDescription('The Username of the Player').setRequired(true))
+            .addStringOption(option => option.setName('user1_role').setDescription('The first user to get winrate with').setRequired(false).addChoices(
+                { name: 'Top', value: 'to' },
+                { name: 'Middle', value: 'mi'},
+                { name: 'Bottom', value: 'bo'},
+                { name: 'Jungle', value: 'ju'},
+                { name: 'Support', value: 'su'},
+            ))
+            .addStringOption(option => option.setName('user1_champ').setDescription('The first user to get winrate with').setRequired(false))
+            .addStringOption(option => option.setName('gametypes').setDescription('The gamemodes to check.').setRequired(false).addChoices(
+                { name: 'Ranked Flex', value: 'Ranked Flex' },
+                { name: 'Ranked Solo/Duo', value: 'Ranked Solo/Duo' },
+                { name: 'Aram', value: 'Aram'},
+                { name: 'Custom', value: 'Custom'},
+                { name: 'Clash', value: 'Clash'},
+                { name: 'Blind', value: 'Blind'},
+                { name: 'All', value: 'All'},
+                { name: 'Ranked', value: 'Ranked'},
+                { name: 'Unranked', value: 'Unranked'},
+            ))
+
+            .addIntegerOption(option => option.setName('history').setDescription('The number of games to check').setRequired(false).addChoices(
+                { name: 'Recent', value: 10 },
+                { name: 'Last 25', value: 25 },
+                { name: 'Last 50', value: 50 },
+            ))
+            .addStringOption(option => option.setName('enemy1').setDescription('The Username of the Player').setRequired(false))
+            .addStringOption(option => option.setName('enemy1_role').setDescription('The first user to get winrate with').setRequired(false).addChoices(
+                { name: 'Top', value: 'to' },
+                { name: 'Middle', value: 'mi'},
+                { name: 'Bottom', value: 'bo'},
+                { name: 'Jungle', value: 'ju'},
+                { name: 'Support', value: 'su'},
+            ))
+            .addStringOption(option => option.setName('enemy1_champ').setDescription('The first user to get winrate with').setRequired(false))
+            .addStringOption(option => option.setName('enemy2').setDescription('The Username of the Player').setRequired(false))
+            .addStringOption(option => option.setName('enemy2_role').setDescription('The first user to get winrate with').setRequired(false).addChoices(
+                { name: 'Top', value: 'to' },
+                { name: 'Middle', value: 'mi'},
+                { name: 'Bottom', value: 'bo'},
+                { name: 'Jungle', value: 'ju'},
+                { name: 'Support', value: 'su'},
+            ))
+            .addStringOption(option => option.setName('enemy2_champ').setDescription('The first user to get winrate with').setRequired(false))
+            .addStringOption(option => option.setName('enemy3').setDescription('The Username of the Player').setRequired(false))
+            .addStringOption(option => option.setName('enemy3_role').setDescription('The first user to get winrate with').setRequired(false).addChoices(
+                { name: 'Top', value: 'to' },
+                { name: 'Middle', value: 'mi'},
+                { name: 'Bottom', value: 'bo'},
+                { name: 'Jungle', value: 'ju'},
+                { name: 'Support', value: 'su'},
+            ))
+            .addStringOption(option => option.setName('enemy3_champ').setDescription('The first user to get winrate with').setRequired(false))
+            .addStringOption(option => option.setName('enemy4').setDescription('The Username of the Player').setRequired(false))
+            .addStringOption(option => option.setName('enemy4_role').setDescription('The first user to get winrate with').setRequired(false).addChoices(
+                { name: 'Top', value: 'to' },
+                { name: 'Middle', value: 'mi'},
+                { name: 'Bottom', value: 'bo'},
+                { name: 'Jungle', value: 'ju'},
+                { name: 'Support', value: 'su'},
+            ))
+            .addStringOption(option => option.setName('enemy4_champ').setDescription('The first user to get winrate with').setRequired(false))
+            .addStringOption(option => option.setName('enemy5').setDescription('The Username of the Player').setRequired(false))
+            .addStringOption(option => option.setName('enemy5_role').setDescription('The first user to get winrate with').setRequired(false).addChoices(
+                { name: 'Top', value: 'to' },
+                { name: 'Middle', value: 'mi'},
+                { name: 'Bottom', value: 'bo'},
+                { name: 'Jungle', value: 'ju'},
+                { name: 'Support', value: 'su'},
+            ))
+            .addStringOption(option => option.setName('enemy5_champ').setDescription('The first user to get winrate with').setRequired(false))
+
+    )
             .addSubcommand(subcommand =>
         subcommand.setName('of_teammates')
             .setDescription('Gets the winrate with up to five teammates')
             .addStringOption(option => option.setName('user1').setDescription('The first user to get winrate with').setRequired(true))
             .addStringOption(option => option.setName('user2').setDescription('The second user of the winrate call').setRequired(true))
+            .addStringOption(option => option.setName('user1_role').setDescription('The first user to get winrate with').setRequired(false).addChoices(
+                { name: 'Top', value: 'to' },
+                { name: 'Middle', value: 'mi'},
+                { name: 'Bottom', value: 'bo'},
+                { name: 'Jungle', value: 'ju'},
+                { name: 'Support', value: 'su'},
+            ))
+            .addStringOption(option => option.setName('user1_champ').setDescription('The first user to get winrate with').setRequired(false))
+            .addStringOption(option => option.setName('user2_role').setDescription('The second user of the winrate call').setRequired(false).addChoices(
+                { name: 'Top', value: 'to' },
+                { name: 'Middle', value: 'mi'},
+                { name: 'Bottom', value: 'bo'},
+                { name: 'Jungle', value: 'ju'},
+                { name: 'Support', value: 'su'},
+            ))
+            .addStringOption(option => option.setName('user2_champ').setDescription('The first user to get winrate with').setRequired(false))
             .addStringOption(option => option.setName('user3').setDescription('The third user of the winrate call').setRequired(false))
+            .addStringOption(option => option.setName('user3_role').setDescription('The third user of the winrate call').setRequired(false).addChoices(
+                { name: 'Top', value: 'to' },
+                { name: 'Middle', value: 'mi'},
+                { name: 'Bottom', value: 'bo'},
+                { name: 'Jungle', value: 'ju'},
+                { name: 'Support', value: 'su'},
+            ))
+            .addStringOption(option => option.setName('user3_champ').setDescription('The first user to get winrate with').setRequired(false))
             .addStringOption(option => option.setName('user4').setDescription('The second user of the winrate call').setRequired(false))
+            .addStringOption(option => option.setName('user4_role').setDescription('The second user of the winrate call').setRequired(false).addChoices(
+                { name: 'Top', value: 'to' },
+                { name: 'Middle', value: 'mi'},
+                { name: 'Bottom', value: 'bo'},
+                { name: 'Jungle', value: 'ju'},
+                { name: 'Support', value: 'su'},
+            ))
+            .addStringOption(option => option.setName('user4_champ').setDescription('The first user to get winrate with').setRequired(false))
             .addStringOption(option => option.setName('user5').setDescription('The second user of the winrate call').setRequired(false))
+            .addStringOption(option => option.setName('user5_role').setDescription('The second user of the winrate call').setRequired(false).addChoices(
+                { name: 'Top', value: 'to' },
+                { name: 'Middle', value: 'mi'},
+                { name: 'Bottom', value: 'bo'},
+                { name: 'Jungle', value: 'ju'},
+                { name: 'Support', value: 'su'},
+            ))
+            .addStringOption(option => option.setName('user5_champ').setDescription('The first user to get winrate with').setRequired(false))
             .addStringOption(option => option.setName('gametypes').setDescription('The gamemodes to check.').setRequired(false).addChoices(
                 { name: 'Ranked Flex', value: 'Ranked Flex' },
                 { name: 'Ranked Solo/Duo', value: 'Ranked Solo/Duo' },
@@ -154,6 +287,33 @@ client.on('interactionCreate', async interaction => {
 
 });
 
+
+let format_users = (interaction_options) => {
+
+    let users = ''
+
+    for (let i = 1; i < 6; i++){
+        let newUser = ''
+
+        if (interaction_options.get(`user${i}`) != null){
+            newUser += interaction_options.getString(`user${i}`)
+        }
+        if (interaction_options.get(`user${i}_role`) != null){
+            newUser += ' playing ' + RoleToTextTwoWayMap.get(interaction_options.getString(`user${i}_role`))
+        }
+        if (interaction_options.get(`user${i}_champ`) != null){
+            newUser += ' as ' + interaction_options.getString(`user${i}_champ`)
+        }
+
+
+        users += newUser === '' ? '' : newUser + ', '
+    }
+
+    return users.substring(0, users.length - 2)
+
+}
+
+
 let handleWinrate = async (interaction) => {
     let subcommand = interaction.options.getSubcommand()
 
@@ -164,7 +324,7 @@ let handleWinrate = async (interaction) => {
         await interaction.deferReply()
 
         try {
-            const result = await getWinrateTogether(interaction.options.getString('user1'), interaction.options.getString('user2'),interaction.options.getString('user3'),interaction.options.getString('user4'),interaction.options.getString('user5'),GameTypeToTextTwoWayMap.revGet(interaction.options.getString('gametypes')), interaction.options.getInteger('history'));
+            const result = await getWinrateTogether(interaction.options);
 
             console.log("Finished awaiting result.")
             console.log(result);
@@ -179,9 +339,11 @@ let handleWinrate = async (interaction) => {
             }
             if (wins + losses !== 0){
 
-                const users = "" + interaction.options.getString('user1') + (interaction.options.getString('user2') == null ? '' : ` and ${interaction.options.getString('user2')}`) + (interaction.options.getString('user3') == null ? '' : ` and ${interaction.options.getString('user3')}`) + (interaction.options.getString('user4') == null ? '' : ` and ${interaction.options.getString('user4')}`) + (interaction.options.getString('user5') == null ? '' : ` and ${interaction.options.getString('user5')}`);
+                let gameType =  interaction.options.getString('gametypes') == null ? '' : "in " + interaction.options.getString('gametypes') + ' games'
+                let users = format_users(interaction.options)
+                let winrate_as_percent = ((wins/(wins + losses)) * 100).toFixed(2)
 
-                final_string = final_string + `\n` + (`The winrate of ${users} ${interaction.options.getString('gametypes') == null ? '' : `in ${interaction.options.getString('gametypes')} games `}is ${wins} wins and ${losses} losses which is **${((wins/(wins + losses)) * 100).toFixed(2)}%**`);
+                final_string = final_string + `\n` + (`The winrate of ${users} ${gameType}is ${wins} wins and ${losses} losses which is **${winrate_as_percent}%**`);
             }
 
             await interaction.editReply(final_string);
@@ -198,17 +360,27 @@ let appendIfNotNull = (UrlSearchParamss, nameOfParam, valueOfParam) => {
     }
 }
 
-const getWinrateTogether = async (user1, user2, user3, user4, user5, gametypes, history) => {
+const getWinrateTogether = async (interaction_options) => {
 
     let paramsObj = new URLSearchParams();
 
-    appendIfNotNull(paramsObj, "TeamMate", user1);
-    appendIfNotNull(paramsObj, "TeamMate", user2);
-    appendIfNotNull(paramsObj, "TeamMate", user3);
-    appendIfNotNull(paramsObj, "TeamMate", user4);
-    appendIfNotNull(paramsObj, "TeamMate", user5);
-    appendIfNotNull(paramsObj, "GameTypes", gametypes);
-    appendIfNotNull(paramsObj, "NoMatches", history);
+    appendIfNotNull(paramsObj, "TeamMate1", interaction_options.getString('user1'));
+    appendIfNotNull(paramsObj, "TeamMate2", interaction_options.getString('user2'));
+    appendIfNotNull(paramsObj, "TeamMate3", interaction_options.getString('user3'));
+    appendIfNotNull(paramsObj, "TeamMate4", interaction_options.getString('user4'));
+    appendIfNotNull(paramsObj, "TeamMate5", interaction_options.getString('user5'));
+    appendIfNotNull(paramsObj, "TeamMatePosition1", interaction_options.getString('user1_role'));
+    appendIfNotNull(paramsObj, "TeamMatePosition2", interaction_options.getString('user2_role'));
+    appendIfNotNull(paramsObj, "TeamMatePosition3", interaction_options.getString('user3_role'));
+    appendIfNotNull(paramsObj, "TeamMatePosition4", interaction_options.getString('user4_role'));
+    appendIfNotNull(paramsObj, "TeamMatePosition5", interaction_options.getString('user5_role'));
+    appendIfNotNull(paramsObj, "TeamMateChampion1", interaction_options.getString('user1_champ'));
+    appendIfNotNull(paramsObj, "TeamMateChampion2", interaction_options.getString('user2_champ'));
+    appendIfNotNull(paramsObj, "TeamMateChampion3", interaction_options.getString('user3_champ'));
+    appendIfNotNull(paramsObj, "TeamMateChampion4", interaction_options.getString('user4_champ'));
+    appendIfNotNull(paramsObj, "TeamMateChampion5", interaction_options.getString('user5_champ'));
+    appendIfNotNull(paramsObj, "GameTypes", GameTypeToTextTwoWayMap.get(interaction_options.getString('gametypes')));
+    appendIfNotNull(paramsObj, "NoMatches", interaction_options.getInteger('history'));
 
     return new Promise(resolve => {
 
